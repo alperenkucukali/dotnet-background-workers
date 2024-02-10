66eeacbab993bddf89c4026986a041655bfee971
alperenkucukali <alperen171195@hotmail.com>
2024-02-11 00:16:16 +0300

    scrape service

--- src/Background.Worker/Services/ScraperService.cs
+++ src/Background.Worker/Services/ScraperService.cs
@@ -1,4 +1,6 @@
-﻿using Background.Worker.Services.Interfaces;
+﻿using Background.Worker.Core.Services.Interfaces;
+using Background.Worker.Services.Interfaces;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,9 +11,25 @@ namespace Background.Worker.Services
 {
     public class ScraperService : IScraperService
     {
-        public Task Start()
+        private readonly ILogger<ScraperService> _logger;
+        private readonly IImageService _imageService;
+
+        public ScraperService(ILogger<ScraperService> logger, IImageService imageService)
         {
-            throw new NotImplementedException();
+            _logger = logger;
+            _imageService = imageService;
+        }
+
+        public async Task Start()
+        {
+            try
+            {
+                //await _imageService.Upload();
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e, "ScraperService raise an error at: {time}", DateTimeOffset.Now);
+            }
         }
     }
 }
