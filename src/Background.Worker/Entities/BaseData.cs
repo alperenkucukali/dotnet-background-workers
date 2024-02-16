6edbea6e2164e7a59b8217c05ee3044ca869c5aa
alperenkucukali <alperen171195@hotmail.com>
2024-02-16 23:19:19 +0300

    docker

--- src/Background.Worker/Entities/BaseData.cs
+++ src/Background.Worker/Entities/BaseData.cs
@@ -8,6 +8,10 @@ namespace Background.Worker.Entities
 {
     public class BaseData<T> where T : BaseEntity
     {
+        public BaseData()
+        {
+            Data = new List<T>();
+        }
         public DateTime Created { get; set; }
         public List<T> Data { get; set; } = null!;
     }
